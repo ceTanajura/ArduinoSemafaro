9e4c1b02721d445d07c95bd0bc7b9b72b5aea817
Carlos Eduardo <eduardotanajura@yahoo.com.br>
2020-08-18 21:12:55 -0300

    Correções de variáveis e declarações

--- ArduinoSemafaro/src/main.cpp
+++ ArduinoSemafaro/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
-int YELLOW=3;      // LED vermelho 11
-int RED=4;   // LED amarelo 12
-int GREEN=2;    // LED verde 13
+int LED_YELLOW=3;      // Pino digital LED Amarelo
+int LED_RED=4;         // Pino digital LED Vermelho  
+int LED_GREEN=2;       // Pino digital LED Verde
 
 int time_RED = 5000;      // 5 SEGUNDOS
 int time_YELLOW = 1500;   // 1,5 SEGUNDOS
@@ -13,29 +13,29 @@ int time_transicao = 200; // 0,2 SEGUNDOS PARA MUDAR O LED
 
 
 void setup() {
-  pinMode(RED,OUTPUT);      // PINAGEM A SER UTILIZADA PARA O LED VERMELHO
-  pinMode(YELLOW,OUTPUT);   // PINAGEM A SER UTILIZADA PARA O LED AMARELO
-  pinMode(GREEN,OUTPUT);    // PINAGEM A SER UTILIZADA PARA O LED VERDE
+  pinMode(LED_RED,OUTPUT);      // PINAGEM A SER UTILIZADA PARA O LED VERMELHO
+  pinMode(LED_YELLOW,OUTPUT);   // PINAGEM A SER UTILIZADA PARA O LED AMARELO
+  pinMode(LED_GREEN,OUTPUT);    // PINAGEM A SER UTILIZADA PARA O LED VERDE
 }
 
 void loop() {
 
  //--------LED GREEN------------
- digitalWrite(GREEN,HIGH);    // LIGAR O LED VERDE
+ digitalWrite(LED_GREEN,HIGH);    // LIGAR O LED VERDE
  delay(time_GREEN);         
- digitalWrite(GREEN,LOW);     // DESLIGAR O LED VERDE
+ digitalWrite(LED_GREEN,LOW);     // DESLIGAR O LED VERDE
  delay(time_transicao);
  
  //--------LED YELLOW-----------
- digitalWrite(YELLOW,HIGH);   // LIGAR O LED AMARELO
+ digitalWrite(LED_YELLOW,HIGH);   // LIGAR O LED AMARELO
  delay(time_YELLOW);            
- digitalWrite(YELLOW,LOW);    // DESLIGAR O LED AMARELO
+ digitalWrite(LED_YELLOW,LOW);    // DESLIGAR O LED AMARELO
  delay(time_transicao);          
          
   //---------LED RED-------------
- digitalWrite(RED,HIGH);      // LIGAR O LED VERMELHO
+ digitalWrite(LED_RED,HIGH);      // LIGAR O LED VERMELHO
  delay(time_RED);            
- digitalWrite(RED,LOW);       // DESLIGAR O LED VERMELHO
+ digitalWrite(LED_RED,LOW);       // DESLIGAR O LED VERMELHO
  delay(time_transicao);     
 
 }
